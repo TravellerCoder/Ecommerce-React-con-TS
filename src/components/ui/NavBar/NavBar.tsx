f8a1cc5c51163dd9d3cd682ec070b6355d1de84c
Traveller Coder <guillermobukowski@gmail.com>
2025-05-03 12:06:24 -0300

    creacion de vista y primeros estilos de carrito

--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,9 +1,18 @@
 import Logo from '../../../assets/logo.svg';
 import CartIcon from '../../../assets/cart.svg';
 import styles from './NavBar.module.css';
+import { useState } from 'react';
+import { CartModal } from '../cartModal';
 
 
 export const NavBar = () => {
+
+    const [showCartModal, setShowCartModal] = useState(false);
+
+    const handleShowCartModal = () => {
+        setShowCartModal(!showCartModal);
+    }
+
   return (
     <div className={styles.navbarContainer}>
         <div className={styles.navbarLogo}>
@@ -14,8 +23,9 @@ export const NavBar = () => {
         </div>
         <div className={styles.navbarCartContainer}>
             <span className={styles.navBarTextAmount}>2</span>
-            <img src={CartIcon} alt="Emoji de carrito" />
+            <img src={CartIcon} alt="Emoji de carrito" onClick={handleShowCartModal} />
         </div>
+        {showCartModal && (<CartModal handleShowCartModal={handleShowCartModal} />)}
     </div>
   )
 }
---
+++ src/components/ui/cartModal/CartModal.tsx
@@ -0,0 +1,42 @@
+import styles from './CartModal.module.css'
+import Close from '../../../assets/close.svg'
+
+export const CartModal = ({handleShowCartModal}) => {
+    return (
+        <div className={styles.modalContainer}>
+            <button className={styles.modalCloseButton} onClick={handleShowCartModal}>
+                <img src={Close} alt="Close" />
+            </button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Delete</th>
+                        <th>Quantity</th>
+                        <th>Add</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>name</td>
+                        <td className={styles.modalModifierButtonContainer}>
+                            <button > -1 </button>
+                        </td>
+                        <td>12</td>
+                        <td className={styles.modalModifierButtonContainer}>
+                            <button > +1 </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <h3>Total : u$d : 599</h3>
+            </div>
+            <div className={styles.modalButtonContainer }>
+                <button >
+                    checkout
+                </button>
+            </div>
+        </div>
+    )
+}
