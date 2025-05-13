26d8ba2188053e787dd2d5e0806e82225acb32be
Traveller Coder <guillermobukowski@gmail.com>
2025-05-13 12:55:17 -0300

    se renderizan los productos en /checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,9 +1,16 @@
 import { Table } from "../../components/ui/Table/Table"
+import styles from './Checkout.module.css'
 
 const Checkout = () => {
   return (
-    <div>
-      < Table />
+    <div className={styles.container}>
+      <h1 className={styles.title}>Finaliza tu compra</h1>
+      <div className={styles.grid}>
+        <div>
+          < Table />
+        </div>
+      </div>
+
     </div>
     
   )
